42b239d7c48995a9fb666371b6536faffa200fd2
huangyan <huangyan091400@gmail.com>
2022-09-16 11:14:17 +0800

    commit1113

--- src/main/java/org/example/observer/ObserverInterf.java
+++ src/main/java/org/example/observer/ObserverInterf.java
@@ -2,4 +2,15 @@ package org.example.observer;
 
 public interface ObserverInterf {
     void onUpdate(ObserverEvent ent);
+
+    /**
+     * description 
+     * @param
+     * @return java.lang.String
+     * @date 2022/9/16 11:13
+     * @author huangyan
+     */
+    default String newMethod() {
+        return "str";
+    }
 }
