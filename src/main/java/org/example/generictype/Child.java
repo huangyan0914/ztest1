f8a8c59c61d7ca9297b8b9543ba776daa20ccd52
huangyan <huangyan091400@gmail.com>
2022-09-16 10:30:05 +0800

    增加方法

--- src/main/java/org/example/generictype/Child.java
+++ src/main/java/org/example/generictype/Child.java
@@ -3,4 +3,11 @@ package org.example.generictype;
 public class Child extends Parent {
     int age;
 
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
